e662f488ddc8afa4abae5489bc5c5e7e175a6020
Berton Adiwidya <139194224+badiwidya@users.noreply.github.com.>
2026-05-30 12:38:09 +0700

    chore: install tanstack eslint plugin and remove legacy .eslintignore

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,8 +4,12 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import pluginQuery from '@tanstack/eslint-plugin-query'
+import pluginRouter from '@tanstack/eslint-plugin-router'
 
 export default defineConfig([
+  ...pluginQuery.configs['flat/recommended'],
+  ...pluginRouter.configs['flat/recommended'],
   globalIgnores(['dist', 'src/client']),
   {
     files: ['**/*.{ts,tsx}'],
